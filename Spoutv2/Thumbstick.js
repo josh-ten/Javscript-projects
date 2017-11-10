1f29f06596343f234b4050f747669fc7230a7df5
s3657249 <s3657249@student.rmit.edu.au>
2017-11-10 20:54:12 +1100

    betterJoystick

--- Spoutv2/Thumbstick.js
+++ Spoutv2/Thumbstick.js
@@ -3,23 +3,32 @@ function Thumbstick(x, y) {
     this.pos = createVector(x, y);
 }
 
-function touchEnded() {
-    // this.direction.mult(0);
-}
-
 Thumbstick.prototype.update = function() {
     if (touches.length > 0) {
         var touchPos = createVector(touches[0].x, touches[0].y);
         this.direction = this.pos.copy().sub(touchPos);
-        this.direction.normalize();
-        player.touchMove(this.direction);
+    } else if (mouseIsPressed) {
+        var mousePos = createVector(mouseX, mouseY);
+        this.direction = this.pos.copy().sub(mousePos);
+    } else {
+        this.direction = createVector(0, 0);
     }
+    var distance = this.direction.mag();
+    this.direction.normalize();
+    player.touchMove(this.direction);
 
-    this.draw();
+    this.draw(distance);
 }
 
-Thumbstick.prototype.draw = function() {
-    fill(0, 150);
-    ellipse(this.pos.x - (this.direction.x * 200), 
-            this.pos.y - (this.direction.y * 200), 50);
+Thumbstick.prototype.draw = function(dist) {
+    ellipseMode(CENTER);
+    fill(0, 100);
+    dist *= 3;
+    ellipse(this.pos.x - (this.direction.x * dist), 
+            this.pos.y - (this.direction.y * dist), 50);
+    noFill();
+    strokeWeight(3);
+    stroke(0, 150);
+    ellipse(this.pos.x - (this.direction.x * dist), 
+            this.pos.y - (this.direction.y * dist), 50);
 }
